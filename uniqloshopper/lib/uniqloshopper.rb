fa29527e970b3eb6977aff68a4dfe62670c37772
jonathanwong110 <45864420+jonathanwong110@users.noreply.github.com>
2019-08-16 00:10:47 -0400

    Delete uniqloshopper.rb

--- uniqloshopper/lib/uniqloshopper.rb
+++
@@ -1,7 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-
-require_relative "./uniqloshopper/version"
-require_relative './uniqloshopper/scraper'
-require_relative './uniqloshopper/cli'
-require_relative './uniqloshopper/shirt'
