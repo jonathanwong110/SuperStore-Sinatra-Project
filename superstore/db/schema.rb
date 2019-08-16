d6c3fc33e1ab2f6997e6cd8995294665ce4efc0c
jonathanwong110 <45864420+jonathanwong110@users.noreply.github.com>
2019-08-16 03:14:23 -0400

    Update schema.rb

--- superstore/db/schema.rb
+++ superstore/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_035052) do
+ActiveRecord::Schema.define(version: 2019_08_16_054319) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -19,10 +19,16 @@ ActiveRecord::Schema.define(version: 2019_08_16_035052) do
     t.integer "user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.string "location"
   end
 
 end
