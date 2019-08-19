931154a459c6a764396a4465715e1f8c1a47a824
jonathanwong110 <45864420+jonathanwong110@users.noreply.github.com>
2019-08-19 01:51:03 -0400

    Update user.rb

--- superstore/app/models/user.rb
+++ superstore/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates :location, presence: true
   has_many :items
+  has_many :messages
 end
