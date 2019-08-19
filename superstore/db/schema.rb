055c2ab4cec441cbaeebdeb07b19b4790ee8ca74
jonathanwong110 <45864420+jonathanwong110@users.noreply.github.com>
2019-08-19 01:55:26 -0400

    Update schema.rb

--- superstore/db/schema.rb
+++ superstore/db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_08_16_054319) do
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
+    t.string "recipient"
   end
 
   create_table "users", force: :cascade do |t|
