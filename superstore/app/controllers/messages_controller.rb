d986e94a751179ee3126de89c4e7a8f49007e88d
jonathanwong110 <45864420+jonathanwong110@users.noreply.github.com>
2019-08-21 17:14:52 -0400

    Update messages_controller.rb

--- superstore/app/controllers/messages_controller.rb
+++ superstore/app/controllers/messages_controller.rb
@@ -19,15 +19,17 @@ class MessagesController < ApplicationController
   
   post '/messages' do
     if is_logged_in?
-      if params[:content].empty? || params[:recipient].empty?
+      if params[:content].empty?
+        flash[:error] = "Please enter a message"
         redirect '/messages/new'
-      else
+      elsif params[:recipient].empty?
+        flash[:error] = "Please enter a recipient"
+        redirect '/messages/new'
+      elsif
         @message = Message.new(:content => params[:content], :recipient => params[:recipient], :user_id => session[:user_id])
-        @message.save
         if @message.save
-          redirect '/messages'
-        else
-          redirect '/messages/error'
+          flash[:message] = "Message was sent to #{@message.recipient}"
+          redirect "/messages/#{@message.id}"
         end
       end
     else
@@ -36,9 +38,14 @@ class MessagesController < ApplicationController
   end
   
   get '/messages/:id' do
+    @message = Message.find(params[:id])
     if is_logged_in?
-      @message = Message.find(params[:id])
-      erb :'/messages/show_message'
+      if @message.user_id == current_user.id || @message.recipient == current_user.username
+        @message = Message.find(params[:id])
+        erb :'/messages/show_message'
+      else
+        redirect :'/messages'
+      end
     else
       redirect '/login'
     end
