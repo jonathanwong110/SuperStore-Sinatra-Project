d7c026895fe25deae6a07abf6b5434852ed196b8
jonathanwong110 <45864420+jonathanwong110@users.noreply.github.com>
2019-08-21 17:15:07 -0400

    Update users_controller.rb

--- superstore/app/controllers/users_controller.rb
+++ superstore/app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   get '/signup' do
     if is_logged_in?
-      redirect '/items'
+      redirect "/users/#{current_user.id}"
     else
       erb :'/users/signup'
     end
@@ -12,15 +12,26 @@ class UsersController < ApplicationController
     @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password], :location => params[:location])
     if @user.save && @user.username != "" && @user.email != ""
       session[:user_id] = @user.id
-      redirect "/items"
-    else
+      flash[:message] = "Sign Up is successful"
+      redirect "/users/#{current_user.id}"
+    elsif params[:username].empty?
+      flash[:error] = "Please enter a username"
+      redirect '/signup'
+    elsif params[:password].empty?
+      flash[:error] = "Please enter a password"
+      redirect '/signup'
+    elsif params[:email].empty?
+      flash[:error] = "Please enter an email"
+      redirect '/signup'
+    else params[:location].empty?
+      flash[:error] = "Please enter a location"
       redirect '/signup'
     end
   end
 
   get '/login' do
     if is_logged_in?
-      redirect '/items'
+      redirect "/users/#{current_user.id}"
     else
       erb :'/users/login'
     end
@@ -30,8 +41,15 @@ class UsersController < ApplicationController
     @user = User.find_by(:username => params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect '/items'
+      redirect "/users/#{current_user.id}"
+    elsif params[:username].empty?
+      flash[:error] = "Please enter a username"
+      redirect '/login'
+    elsif params[:password].empty?
+      flash[:error] = "Please enter a password"
+      redirect '/login'
     else
+      flash[:error] = "Incorrect username and/or password"
       redirect '/login'
     end
   end
@@ -48,7 +66,7 @@ class UsersController < ApplicationController
       @users = User.all
       erb :'/users/users'
     else
-      redirect '/users/login'
+      redirect '/login'
     end
   end
   
@@ -59,6 +77,52 @@ class UsersController < ApplicationController
       erb :'/users/show_user'
     else
       redirect '/login'
+    end
+  end
+  
+  get '/users/:id/edit' do
+    @user = User.find(params[:id])
+    if is_logged_in? && @user.id == current_user.id
+      @user.save
+      erb :'/users/edit_user'
+    else
+      redirect "/users/#{@user.id}"
+    end
+  end
+  
+  post '/users/:id/edit' do
+    @user = User.find(params[:id])
+     if !params[:email].empty? && !params[:password_digest].empty? && !params[:location].empty?
+      @user.update(:email => params[:email], :password => params[:password_digest], :location => params[:location])
+      @user.save
+      flash[:message] = "Update is successful"
+      redirect "/users/#{@user.id}"
+     elsif params[:email].empty?
+      flash[:error] = "Please enter an email"
+     elsif params[:location].empty?
+      flash[:error] = "Please enter a location"
+      redirect "/users/#{@user.id}/edit"
+     elsif params[:password_digest].empty?
+      flash[:error] = "Please enter a password"
+      redirect "/users/#{@user.id}/edit"
+     else params[:location].empty?
+      flash[:error] = "Please enter a location"
+      redirect "/users/#{@user.id}/edit"
+    end
+  end
+
+  post '/users/:id/delete' do
+    @user = User.find(params[:id])
+    if is_logged_in?
+      if @user.id == current_user.id
+        @user.delete
+        flash[:message] = "Deletion of #{@user.username.capitalize} is successful"
+        redirect '/users'
+      else
+        redirect '/users'
+      end
+    else
+      redirect "/login"
     end
   end
   
